551a797890cebdc9b03b8e8c20289260e71a4816
LaniJeong <123913969+LaniJeong@users.noreply.github.com>
2023-02-14 17:11:09 +0900

    12일차_2차 저장

--- Day02/ds02_simple_linked.py
+++ Day02/ds02_simple_linked.py
@@ -45,11 +45,43 @@ def inserNode(findData, insertData):
             pre.link = node
             return
 
+# 노드 삭제
+def deletNode(deletData):
+    global memory, pre, current, head
+
+    if head.data == deletData:
+        current = head
+        head = head.link
+        del(current)
+        return
+
+    current = head
+    while current.link != None:
+        pre = current            # 모두 첫번째 노드 가리킴
+        current = current.link   # 두번째 노드를 가리킴
+        if current.data == deletData:
+            pre.link = current.link     # current가 가리키는 노드를 pre가 가리키도록(삭제)
+            del(current)
+            return
 # current.link == Node 까지 온것
     node = Node()
     node.data = insertData
     current.link = node
     return 
+
+# 노드검색
+def findNode(findData):
+    global memory, pre, current, head
+
+    current = head    # 첫번째 노드부터
+    if current.data == findData:
+        return current
+    while current.link != None:
+        current = current.link   # 다음노드
+        if current.data == findData:
+            return current
+
+    return Node()   # 빈노드 반환
 
 if __name__ == '__main__':
     node = Node()
@@ -66,7 +98,41 @@ if __name__ == '__main__':
 
     printNodes(head)       # 전체출력
 
+    print('노드추가 ----')
 
+    inserNode('주헌', '화사')  # 맨 앞에 화사노드 추가
+    printNodes(head)
+
+    inserNode('형원', '솔라')
+    printNodes(head)
+
+    inserNode('아이엠', '문별')
+    printNodes(head)
+
+    print('노드 삭제 ----')
+    
+    deletNode('화사')
+    printNodes(head)
+
+    deletNode('문별')
+    printNodes(head)
+
+    deletNode('주헌')
+    printNodes(head)
+
+    print('노드 검색 ---')
+
+    result = findNode('아이엠')
+    if result.data != None:
+        print('데이터를 찾았습니다.')
+    else:
+        print('검색한 데이터가 없습니다.')
+
+    result = findNode('주헌')
+    if result.data != None:
+        print('데이터를 찾았습니다.')
+    else:
+        print('검색한 데이터가 없습니다.')
 
 
 
---
+++ Day02/ds04_stack.py
@@ -0,0 +1,80 @@
+# 스택 전체 구현
+# global 변수
+SIZE = 0
+stack = []
+top = -1
+
+# 스택이 꽉 찼는지 여부확인
+def isStackFull():
+    global SIZE, stack, top # 전역변수를 그대로 함수에서도 쓰겠다.
+
+    if (top >= SIZE-1):   # 다 찼다는 말
+        return True
+    else:
+        return False
+
+# 스택이 비어있는지 여부확인
+def isStackEmpty():
+    global SIZE, stack, top # 전역변수를 그대로 함수에서도 쓰겠다.
+
+    if (top == -1):   # 다 비어있다
+        return True
+    else:
+        return False
+
+# 스택에 데이터 추가
+def push(data):
+    global SIZE, stack, top
+    if (isStackFull()):
+        print('stack is full')
+    else:
+        top +=1
+        stack[top] = data
+
+# 스택에서 데이터 추출(pop)
+def pop():
+    global SIZE, stack, top
+    if (isStackEmpty()):
+        print('stack is empty')
+    else:
+        data = stack[top]
+        stack[top] = None
+        top -= 1
+        return data
+
+# top의 데이터 확인(peek)
+def peek():
+    global SIZE, stack, top
+    if isStackEmpty():
+        print('stack is empty')
+        return None
+    else:
+        return stack[top]
+
+'''
+# 메인 엔트리
+if __name__ == '__main__':
+    SIZE = int(input('스택사이즈 입력 >> '))
+    stack = [None for _ in range(SIZE)]
+
+    while True:
+        select = input('삽입(I)/추출(E)/확인(V)/종료(X) >> ')
+        if select.lower() == 'x':             # 종료
+            break
+        elif select.lower() == 'i':           # 삽입
+            data = input('추가할 데이터 >> ')
+            push(data)
+            print(f'스택상태 : {stack}')
+        elif select.lower() == 'e':           # 추출
+            data = pop()
+            print(f'추출 데이터 : {data}')
+            print(f'스택상태 : {stack}')
+        elif select.lower() == 'v':
+            data = peek()
+            print(f'확인 데이터 : {data}')
+            print(f'스택상태 : {stack}')      # 확인
+        else:
+            continue
+
+    print('스택 프로그램 종료')
+'''
